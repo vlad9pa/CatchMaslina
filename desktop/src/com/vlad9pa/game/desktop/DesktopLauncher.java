3364b630b51daee5cb7aa827ce626306af9b6b05
Vlad9pa <colt9pa@gmail.com>
2016-05-05 14:42:27 +0300

    EmptyProject

---
+++ desktop/src/com/vlad9pa/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.vlad9pa.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.vlad9pa.game.catchMaslina;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new catchMaslina(), config);
+	}
+}
